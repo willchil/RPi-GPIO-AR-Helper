8c26a9902347fca146e900bac5e5aba09e217f33
William Chilcote <wnchilcote@ucdavis.edu>
2021-05-16 19:21:02 -0700

    Initial Commit

---
+++ RPi GPIO AR Helper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RPi GPIO AR Helper
+//
+//  Created by William Chilcote on 5/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RPi GPIO AR Helper/RPi_GPIO_AR_HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPi_GPIO_AR_HelperApp.swift
+//  RPi GPIO AR Helper
+//
+//  Created by William Chilcote on 5/16/21.
+//
+
+import SwiftUI
+
+@main
+struct RPi_GPIO_AR_HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
